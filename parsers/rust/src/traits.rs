623fed11beee8457632a67f384971113c61d8fc5
Johannes Graner <johannes.graner@gmail.com>
2023-05-07 13:59:52 +0200

    Move traits to separate module

---
+++ parsers/rust/src/traits.rs
@@ -0,0 +1,24 @@
+use serde_json::{json, Value};
+
+pub trait To5etools {
+    fn to_5etools(&self) -> Value;
+    fn to_5etools_spell(&self) -> Value {
+        self.to_5etools()
+    }
+}
+
+impl<T: To5etools + Copy> To5etools for Vec<T> {
+    fn to_5etools(&self) -> Value {
+        self.iter()
+            .map(|value| value.to_5etools())
+            .collect::<Value>()
+            .into()
+    }
+
+    fn to_5etools_spell(&self) -> Value {
+        self.iter()
+            .map(|value| value.to_5etools_spell())
+            .collect::<Value>()
+            .into()
+    }
+}
