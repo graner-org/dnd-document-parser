51aec57e4d48c73352d140491b9f08edcf87b5e8
Johannes Graner <johannes.graner@gmail.com>
2023-05-07 13:54:17 +0200

    5etools serialization for spells

--- parsers/rust/src/main.rs
+++ parsers/rust/src/main.rs
@@ -8,7 +8,11 @@ mod models;
 
 fn main() {
     let s = Spell {
-        name: "Blade of Disaster".to_owned(),
+        source: Source {
+            source_book: "PHB",
+            page: 0,
+        },
+        name: "Blade of Disaster",
         level: 9,
         school: MagicSchool::Conjuration,
         casting_time: CastingTime {
@@ -35,17 +39,14 @@ fn main() {
                 consumed: false
             }),
         },
-        damage_type: Some(DamageType::Force),
-        description: [
+        damage_type: Some(vec![DamageType::Force]),
+        description: vec![
             "You create a blade-shaped planar rift about 3 feet long in an unoccupied space you can see within range. The blade lasts for the duration. When you cast this spell, you can make up to two melee spell attacks with the blade, each one against a creature, loose object, or structure within 5 feet of the blade. On a hit, the target takes {@damage 4d12} force damage. This attack scores a critical hit if the number on the {@dice d20} is 18 or higher. On a critical hit, the blade deals an extra {@damage 8d12} force damage (for a total of {@damage 12d12} force damage).",
 			"As a bonus action on your turn, you can move the blade up to 30 feet to an unoccupied space you can see and then make up to two melee spell attacks with it again.",
 			"The blade can harmlessly pass through any barrier, including a {@spell wall of force}."
-        ].map(|s| s.to_owned()).to_vec(),
+        ],
         at_higher_levels: None,
         classes: vec!(Classes::Wizard, Classes::Sorcerer),
     };
-    println!("{:?}", s);
-    println!("{}", s.range.to_5etools());
-    println!("{}", s.components.to_5etools());
-    println!("{}", s.duration.to_5etools());
+    println!("{}", s.to_5etools_spell());
 }
