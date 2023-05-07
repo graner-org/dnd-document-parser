5cc71c8d7f955e5bbd1e57dfb8ddc04013c2e682
Johannes Graner <johannes.graner@gmail.com>
2023-05-07 13:59:52 +0200

    Move traits to separate module

--- parsers/rust/src/models/common.rs
+++ parsers/rust/src/models/common.rs
@@ -1,29 +1,6 @@
 use serde_json::{json, Value};
 
-pub trait SpellElement {}
-
-pub trait To5etools {
-    fn to_5etools(&self) -> Value;
-    fn to_5etools_spell(&self) -> Value {
-        self.to_5etools()
-    }
-}
-
-impl<T: To5etools + Copy> To5etools for Vec<T> {
-    fn to_5etools(&self) -> Value {
-        self.iter()
-            .map(|value| value.to_5etools())
-            .collect::<Value>()
-            .into()
-    }
-
-    fn to_5etools_spell(&self) -> Value {
-        self.iter()
-            .map(|value| value.to_5etools_spell())
-            .collect::<Value>()
-            .into()
-    }
-}
+use crate::traits::To5etools;
 
 #[allow(dead_code)]
 #[derive(Debug)]
