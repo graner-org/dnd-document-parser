9bc8df03bedd2e016d4a11fc057acd606b553194
johannes-graner <johannes.graner@gmail.com>
2023-05-19 20:18:33 +0200

    Informative error definitions

---
+++ parsers/rust/src/error.rs
@@ -0,0 +1,48 @@
+use serde_json::Error as JSONError;
+use std::io::Error as IOError;
+
+#[derive(Debug)]
+pub struct ParseError {
+    pub string: String,
+    pub parsing_step: String,
+    pub problem: Option<String>,
+}
+
+#[derive(Debug)]
+pub struct OutOfBoundsError {
+    pub array: Vec<String>,
+    pub parsing_step: String,
+    pub problem: Option<String>,
+}
+
+#[derive(Debug)]
+pub enum Error {
+    IO(IOError),
+    JSON(JSONError),
+    OutOfBounds(OutOfBoundsError),
+    Parse(ParseError),
+}
+
+impl From<IOError> for Error {
+    fn from(value: IOError) -> Self {
+        Error::IO(value)
+    }
+}
+
+impl From<JSONError> for Error {
+    fn from(value: JSONError) -> Self {
+        Error::JSON(value)
+    }
+}
+
+impl From<ParseError> for Error {
+    fn from(value: ParseError) -> Self {
+        Error::Parse(value)
+    }
+}
+
+impl From<OutOfBoundsError> for Error {
+    fn from(value: OutOfBoundsError) -> Self {
+        Error::OutOfBounds(value)
+    }
+}
