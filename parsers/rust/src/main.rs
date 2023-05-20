3942ab525b0759b26dd38e75e3ea0f70f7402f40
johannes-graner <johannes.graner@gmail.com>
2023-05-20 16:11:05 +0200

    Set up main function to parse multiple spells

--- parsers/rust/src/main.rs
+++ parsers/rust/src/main.rs
@@ -1,20 +1,22 @@
-use std::fs;
-
 use dnd_document_parser::models::common::Source;
-use dnd_document_parser::parsers::spells::*;
-use dnd_document_parser::utils::traits::To5etools;
+use dnd_document_parser::parsers::spells::parse_gm_binder;
+use itertools::Itertools;
 
 fn main() {
     let source_file = format!(
-        "{}/resources/test/spells/gm_binder_input.html",
+        "{}/resources/test/spells/gm_binder_input_multiple.html",
         env!("CARGO_MANIFEST_DIR")
     );
     let source_book = Source {
         source_book: "book",
         page: 0,
     };
-    let spell = fs::read_to_string(source_file.clone())
+    let source = std::fs::read_to_string(source_file.clone())
         .unwrap_or_else(|_| panic!("Failed to read {source_file}"));
-    let parsed_spell = parse_gm_binder(spell, source_book).map(|spell| spell.to_5etools_spell());
-    println!("{:?}", parsed_spell);
+    let parsed_spells = source
+        .split("\n\n")
+        .flat_map(|spell_str| parse_gm_binder(spell_str.to_owned(), source_book.clone()))
+        .map(|spell| spell.name)
+        .collect_vec();
+    println!("{:?}", parsed_spells);
 }
