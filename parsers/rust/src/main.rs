6a68cf33873768a52d170047e07a5f73f1d9e99b
johannes-graner <johannes.graner@gmail.com>
2023-05-20 17:52:50 +0200

    Support for reading a directory.
    Closed: #57

--- parsers/rust/src/main.rs
+++ parsers/rust/src/main.rs
@@ -1,8 +1,10 @@
+use std::fs;
 use std::path::PathBuf;
 
 use clap::Parser;
 use dnd_document_parser::models::common::Source;
 use dnd_document_parser::parsers::spells::parse_gm_binder;
+use dnd_document_parser::utils::error::Error;
 use itertools::Itertools;
 
 #[derive(Parser)]
@@ -14,23 +16,43 @@ use itertools::Itertools;
 
 {all-args} {tab}")]
 struct Cli {
-    /// Path to file that will be parsed
-    input_file: PathBuf,
+    /// Path to file or directory that will be parsed
+    input_path: PathBuf,
 }
 
-fn main() {
+// TODO: Better error messages
+fn find_html_files(path: PathBuf) -> Result<Vec<PathBuf>, Error> {
+    if path.is_file() {
+        return match path.extension().map(|ext| ext.to_str()).flatten() {
+            Some("html") => Ok(vec![path]),
+            _ => Ok(vec![]),
+        };
+    }
+    fs::read_dir(path)?
+        .map_ok(|path| find_html_files(path.path()))
+        .flatten()
+        .fold_ok(vec![], |acc, paths| [acc, paths].concat())
+}
+
+fn main() -> Result<(), Error> {
     let args = Cli::parse();
-    let source_file = args.input_file;
+    let sources = find_html_files(args.input_path)?;
     let source_book = Source {
         source_book: "book",
         page: 0,
     };
-    let source = std::fs::read_to_string(source_file.clone())
-        .unwrap_or_else(|_| panic!("Failed to read {source_file:?}"));
-    let parsed_spells = source
-        .split("\n\n")
-        .flat_map(|spell_str| parse_gm_binder(spell_str.to_owned(), source_book.clone()))
-        .map(|spell| spell.name)
-        .collect_vec();
-    println!("{:?}", parsed_spells);
+    for source_path in sources {
+        let source = fs::read_to_string(source_path.clone())?;
+        let parsed_spells = source
+            .split("\n\n")
+            .flat_map(|spell_str| parse_gm_binder(spell_str.to_owned(), source_book.clone()))
+            .map(|spell| spell.name)
+            .collect_vec();
+        println!("{source_path:?}:");
+        for spell in parsed_spells.clone() {
+            println!("\t{spell}");
+        }
+        println!("Spells found: {}", parsed_spells.len());
+    }
+    Ok(())
 }
