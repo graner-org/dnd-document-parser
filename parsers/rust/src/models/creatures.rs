14eeb7184ab5c761fe84f49b1020307293d1eb4d
johannes-graner <johannes.graner@gmail.com>
2023-06-10 11:47:50 +0200

    Speed

--- parsers/rust/src/models/creatures.rs
+++ parsers/rust/src/models/creatures.rs
@@ -1,6 +1,8 @@
 use crate::utils::traits::To5etools;
 use serde_json::{json, Value};
 
+use super::common::merge_json;
+
 #[cfg(test)]
 mod tests;
 
@@ -108,5 +110,57 @@ impl To5etools for HitPoints {
             "average": self.average,
             "formula": self.formula.to_5etools_base(),
         })
+    }
+}
+
+#[allow(dead_code)]
+#[derive(Debug, Clone, PartialEq, Eq)]
+pub struct FlySpeed {
+    pub speed: u16,
+    pub hover: bool,
+}
+
+impl To5etools for FlySpeed {
+    fn to_5etools_base(&self) -> Value {
+        if self.hover {
+            json!({
+                "number": self.speed,
+                "condition": "(hover)",
+            })
+        } else {
+            json!(self.speed)
+        }
+    }
+}
+
+#[allow(dead_code)]
+#[derive(Debug, Clone, PartialEq, Eq)]
+pub struct Speed {
+    pub walk: u16,
+    pub burrow: Option<u16>,
+    pub climb: Option<u16>,
+    pub crawl: Option<u16>,
+    pub fly: Option<FlySpeed>,
+    pub swim: Option<u16>,
+}
+
+impl To5etools for Speed {
+    fn to_5etools_base(&self) -> Value {
+        let empty_json = json!({});
+        merge_json(vec![
+            json!({"walk": self.walk}),
+            self.burrow
+                .map_or_else(|| empty_json.clone(), |speed| json!({ "burrow": speed })),
+            self.climb
+                .map_or_else(|| empty_json.clone(), |speed| json!({ "climb": speed })),
+            self.crawl
+                .map_or_else(|| empty_json.clone(), |speed| json!({ "crawl": speed })),
+            self.fly.as_ref().map_or_else(
+                || empty_json.clone(),
+                |fly_speed| json!({ "fly": fly_speed.to_5etools_base() }),
+            ),
+            self.swim
+                .map_or_else(|| empty_json.clone(), |speed| json!({ "swim": speed })),
+        ])
     }
 }
--- parsers/rust/src/models/creatures/tests.rs
+++ parsers/rust/src/models/creatures/tests.rs
@@ -1,7 +1,7 @@
 use serde_json::json;
 
 use crate::{
-    models::creatures::{HitPoints, HitPointsFormula},
+    models::creatures::{FlySpeed, HitPoints, HitPointsFormula, Speed},
     utils::traits::To5etools,
 };
 
@@ -19,4 +19,65 @@ fn hit_points() {
         .to_5etools_base(),
         json!({"average": 91, "formula": "14d8 + 28"})
     )
+}
+
+#[test]
+fn speed() {
+    assert_eq!(
+        Speed {
+            walk: 30,
+            burrow: None,
+            climb: None,
+            crawl: None,
+            fly: None,
+            swim: None,
+        }
+        .to_5etools_base(),
+        json!({"walk": 30})
+    );
+
+    assert_eq!(
+        Speed {
+            walk: 30,
+            burrow: Some(40),
+            climb: None,
+            crawl: None,
+            fly: None,
+            swim: Some(10),
+        }
+        .to_5etools_base(),
+        json!({"walk": 30, "burrow": 40, "swim": 10})
+    );
+
+    assert_eq!(
+        Speed {
+            walk: 0,
+            burrow: None,
+            climb: None,
+            crawl: None,
+            fly: Some(FlySpeed {
+                speed: 60,
+                hover: false
+            }),
+            swim: Some(10),
+        }
+        .to_5etools_base(),
+        json!({"walk": 0, "fly": 60, "swim": 10})
+    );
+
+    assert_eq!(
+        Speed {
+            walk: 0,
+            burrow: None,
+            climb: None,
+            crawl: None,
+            fly: Some(FlySpeed {
+                speed: 60,
+                hover: true
+            }),
+            swim: Some(10),
+        }
+        .to_5etools_base(),
+        json!({"walk": 0, "fly": { "number": 60, "condition": "(hover)" }, "swim": 10})
+    );
 }
