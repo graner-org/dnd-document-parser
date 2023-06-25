37d867a2c44992f33e49c2a80ced773b78d7f6f7
johannes-graner <johannes.graner@gmail.com>
2023-06-25 20:36:54 +0200

    First group Parsing
    Closed: #94

--- parsers/rust/src/parsers/creatures.rs
+++ parsers/rust/src/parsers/creatures.rs
@@ -1,5 +1,15 @@
 use itertools::Itertools;
 
+use crate::{
+    models::{
+        common::{Alignment, AlignmentAxis, AlignmentAxisMoral, AlignmentAxisOrder},
+        creatures::{CreatureType, CreatureTypeEnum, Size},
+    },
+    utils::error::{Error, OutOfBoundsError, ParseError},
+};
+
+type Name = String;
+
 #[cfg(test)]
 mod tests;
 
@@ -26,4 +36,203 @@ fn extract_stat_blocks(document: String) -> Vec<Vec<String>> {
             }
         })
         .collect_vec()
+}
+
+fn parse_first_group(
+    first_group: Vec<String>,
+) -> Result<(Name, Size, CreatureType, Alignment), Error> {
+    fn clean_name(name: &String) -> Result<Name, ParseError> {
+        name.strip_prefix("## ")
+            .ok_or_else(|| ParseError {
+                string: name.to_string(),
+                parsing_step: "Name".to_string(),
+                problem: Some("Name line does not start with `## `".to_string()),
+            })
+            .map(ToString::to_string)
+    }
+
+    let (name, second_line) = match &first_group[..] {
+        [name_line, second_line] => Ok((
+            clean_name(name_line)?,
+            second_line.to_lowercase().replacen('*', "", 2),
+        )),
+        _ => Err(OutOfBoundsError {
+            array: first_group.clone(),
+            index: first_group.len() as u32,
+            parsing_step: "First group".to_string(),
+            problem: Some("Expected array of length 2".to_string()),
+        }),
+    }?;
+
+    let (size_type, alignment) = match second_line.splitn(2, ", ").collect_vec()[..] {
+        [size_type, alignment] => Ok((size_type, Alignment::try_from(alignment)?)),
+        _ => Err(ParseError {
+            string: second_line.clone(),
+            parsing_step: "Separating size and type from alignment".to_string(),
+            problem: Some("No `, ` separation found.".to_string()),
+        }),
+    }?;
+
+    let (size, creature_type) = match size_type.splitn(2, ' ').collect_vec()[..] {
+        [size, creature_type] => Ok((
+            Size::try_from(size)?,
+            CreatureType::try_from(creature_type)?,
+        )),
+        _ => Err(ParseError {
+            string: size_type.to_string(),
+            parsing_step: "Separating size and creature type".to_string(),
+            problem: Some("Could not separate by ` `".to_string()),
+        }),
+    }?;
+
+    Ok((name, size, creature_type, alignment))
+}
+
+impl TryFrom<&str> for Size {
+    type Error = ParseError;
+    fn try_from(value: &str) -> Result<Self, Self::Error> {
+        use Size::*;
+        match value {
+            "tiny" => Ok(Tiny),
+            "small" => Ok(Small),
+            "medium" => Ok(Medium),
+            "large" => Ok(Large),
+            "huge" => Ok(Huge),
+            "gargantuan" => Ok(Gargantuan),
+            _ => Err(ParseError {
+                string: value.to_string(),
+                parsing_step: "Size".to_string(),
+                problem: None,
+            }),
+        }
+    }
+}
+
+impl TryFrom<&str> for CreatureTypeEnum {
+    type Error = ParseError;
+    fn try_from(value: &str) -> Result<Self, Self::Error> {
+        use CreatureTypeEnum::*;
+        match value {
+            "aberration" => Ok(Aberration),
+            "beast" => Ok(Beast),
+            "celestial" => Ok(Celestial),
+            "construct" => Ok(Construct),
+            "dragon" => Ok(Dragon),
+            "elemental" => Ok(Elemental),
+            "fey" => Ok(Fey),
+            "fiend" => Ok(Fiend),
+            "giant" => Ok(Giant),
+            "humanoid" => Ok(Humanoid),
+            "monstrosity" => Ok(Monstrosity),
+            "ooze" => Ok(Ooze),
+            "plant" => Ok(Plant),
+            "undead" => Ok(Undead),
+            _ => Err(ParseError {
+                string: value.to_string(),
+                parsing_step: "Main creature type".to_string(),
+                problem: None,
+            }),
+        }
+    }
+}
+
+impl TryFrom<&str> for CreatureType {
+    type Error = ParseError;
+    fn try_from(value: &str) -> Result<Self, Self::Error> {
+        match value.replace('/', " ").splitn(2, ' ').collect_vec()[..] {
+            [main_type] => Ok(CreatureType {
+                main_type: main_type.try_into()?,
+                subtypes: None,
+            }),
+            [main_type, subtypes] => Ok(CreatureType {
+                main_type: main_type.try_into()?,
+                subtypes: Some(
+                    subtypes
+                        .replace('(', "")
+                        .replace(')', "")
+                        .split(", ")
+                        .map(ToString::to_string)
+                        .collect_vec(),
+                ),
+            }),
+            _ => Err(ParseError {
+                string: value.to_string(),
+                parsing_step: "Creature type".to_string(),
+                problem: None,
+            }),
+        }
+    }
+}
+
+impl TryFrom<&str> for AlignmentAxisMoral {
+    type Error = ParseError;
+    fn try_from(value: &str) -> Result<Self, Self::Error> {
+        use AlignmentAxisMoral::{Evil, Good, Neutral};
+        match value {
+            "good" => Ok(Good),
+            "neutral" => Ok(Neutral),
+            "evil" => Ok(Evil),
+            _ => Err(ParseError {
+                string: value.to_string(),
+                parsing_step: "AlignmentAxisMoral".to_string(),
+                problem: None,
+            }),
+        }
+    }
+}
+
+impl TryFrom<&str> for AlignmentAxisOrder {
+    type Error = ParseError;
+    fn try_from(value: &str) -> Result<Self, Self::Error> {
+        use AlignmentAxisOrder::{Chaotic, Lawful, Neutral};
+        match value {
+            "lawful" => Ok(Lawful),
+            "neutral" => Ok(Neutral),
+            "chaotic" => Ok(Chaotic),
+            _ => Err(ParseError {
+                string: value.to_string(),
+                parsing_step: "AlignmentAxisOrder".to_string(),
+                problem: None,
+            }),
+        }
+    }
+}
+
+impl TryFrom<&str> for AlignmentAxis {
+    type Error = ParseError;
+    fn try_from(value: &str) -> Result<Self, Self::Error> {
+        use AlignmentAxis::{Moral, Order};
+        value
+            .try_into()
+            .map(Order)
+            .or_else(|_| value.try_into().map(Moral))
+    }
+}
+
+impl TryFrom<&str> for Alignment {
+    type Error = ParseError;
+    fn try_from(value: &str) -> Result<Self, Self::Error> {
+        match value
+            .splitn(3, ' ')
+            .filter(|word| !word.contains("alignment"))
+            .collect_vec()[..]
+        {
+            ["any"] => Ok(Self::Any),
+            ["unaligned"] => Ok(Self::Unaligned),
+            ["neutral"] => Ok(Self::TwoAxes {
+                order: AlignmentAxisOrder::Neutral,
+                moral: AlignmentAxisMoral::Neutral,
+            }),
+            ["any", single_axis] => Ok(Self::OneAxis(single_axis.try_into()?)),
+            [order_axis, moral_axis] => Ok(Self::TwoAxes {
+                order: order_axis.try_into()?,
+                moral: moral_axis.try_into()?,
+            }),
+            _ => Err(ParseError {
+                string: value.to_string(),
+                parsing_step: "Alignment".to_string(),
+                problem: None,
+            }),
+        }
+    }
 }
--- parsers/rust/src/parsers/creatures/tests.rs
+++ parsers/rust/src/parsers/creatures/tests.rs
@@ -4,7 +4,13 @@ use std::{
     io::{read_to_string, BufReader},
 };
 
-use crate::parsers::creatures::extract_stat_blocks;
+use crate::{
+    models::{
+        common::{Alignment, AlignmentAxis, AlignmentAxisMoral, AlignmentAxisOrder},
+        creatures::{CreatureType, CreatureTypeEnum, Size},
+    },
+    parsers::creatures::{extract_stat_blocks, parse_first_group},
+};
 
 #[test]
 fn extract_stat_blocks_test() {
@@ -24,5 +30,90 @@ fn extract_stat_blocks_test() {
     assert_eq!(
         first_extracted_block, expected_first_block,
         "Stat block not parsed correctly.",
+    )
+}
+
+#[test]
+fn parse_first_group_test() {
+    let first_group = vec![
+        "## test".to_string(),
+        "*Medium beast, unaligned*".to_string(),
+    ];
+    let expected_result = Ok((
+        "test".to_string(),
+        Size::Medium,
+        CreatureType {
+            main_type: CreatureTypeEnum::Beast,
+            subtypes: None,
+        },
+        Alignment::Unaligned,
+    ));
+
+    assert_eq!(parse_first_group(first_group), expected_result);
+
+    assert!(parse_first_group(vec![
+        "Unparsable entity".to_string(),
+        "With multiple lines".to_string()
+    ])
+    .is_err());
+}
+
+#[test]
+fn creature_type() {
+    assert_eq!(
+        "fiend (demon)".try_into(),
+        Ok(CreatureType {
+            main_type: CreatureTypeEnum::Fiend,
+            subtypes: Some(vec!["demon".to_string()])
+        })
+    );
+
+    assert_eq!(
+        "fiend/undead".try_into(),
+        Ok(CreatureType {
+            main_type: CreatureTypeEnum::Fiend,
+            subtypes: Some(vec!["undead".to_string()])
+        })
+    );
+
+    assert_eq!(
+        "fiend".try_into(),
+        Ok(CreatureType {
+            main_type: CreatureTypeEnum::Fiend,
+            subtypes: None,
+        })
+    );
+}
+
+#[test]
+fn alignment() {
+    use Alignment::{Any, OneAxis, TwoAxes, Unaligned};
+    use AlignmentAxis::Order;
+    use AlignmentAxisMoral::Evil;
+    use AlignmentAxisOrder::Chaotic;
+
+    assert_eq!(
+        "chaotic evil".try_into(),
+        Ok(TwoAxes {
+            order: Chaotic,
+            moral: Evil,
+        })
+    );
+
+    assert_eq!(
+        "any chaotic alignment".try_into(),
+        Ok(OneAxis(Order(Chaotic)))
+    );
+
+    assert_eq!("any alignment".try_into(), Ok(Any));
+
+    assert_eq!("unaligned".try_into(), Ok(Unaligned));
+
+    assert_eq!(
+        "neutral".try_into(),
+        Ok(TwoAxes {
+            order: AlignmentAxisOrder::Neutral,
+            moral: AlignmentAxisMoral::Neutral,
+        })
     )
 }
